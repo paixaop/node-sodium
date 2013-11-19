e48c6be64fa2c6c75fa939a24839cd8c35b59365
Pedro Paixao <paixaop@gmail.com>
2013-11-18 21:24:52 -0500

    update function names

--- test/test_crypto_version.js
+++ test/test_crypto_version.js
@@ -8,18 +8,18 @@ var sodium = require('../build/Release/sodium');
 
 describe('Version', function() {
     it('should return a string', function(done) {
-        var v = sodium.version();
+        var v = sodium.sodium_version_string();
         v.should.have.type('string');
         done();
     })
 
     it('Minor should return an integer', function(done) {
-        sodium.version_minor().should.have.type('number');
+        sodium.sodium_library_version_minor().should.have.type('number');
         done();
     });
 
     it('Major should return an integer', function(done) {
-        sodium.version_major().should.have.type('number');
+        sodium.sodium_library_version_major().should.have.type('number');
         done();
     });
 });
