84482df7d8ea082411dbc41e845f2238687faaf2
Pedro Paixao <paixaop@gmail.com>
2013-11-19 19:41:29 -0500

    reformat

--- examples/low-level-api.js
+++ examples/low-level-api.js
@@ -1,5 +1,6 @@
 var sodium = require('sodium').api;
 
+
 // Generate keys
 var sender = sodium.crypto_box_keypair();
 var receiver = sodium.crypto_box_keypair();
@@ -13,9 +14,10 @@ var plainText = new Buffer('this is a message');
 var cipherMsg = sodium.crypto_box(plainText, nonce, receiver.publicKey, sender.secretKey);
 
 // Decrypt
-var plainBuffer = sodium.crypto_box_open(cipherMsg,nonce,sender.publicKey, receiver.secretKey);
+var plainBuffer = sodium.crypto_box_open(cipherMsg, nonce, sender.publicKey,
+	receiver.secretKey);
 
 // We should get the same plainText!
-if( plainBuffer.toString() == plainText) {
-    console.log("Message decrypted correctly");
+if (plainBuffer.toString() == plainText) {
+	console.log("Message decrypted correctly");
 }
