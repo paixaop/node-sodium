375601bb91784f2649c2312936b9bb88d994b447
Pedro Paixao <paixaop@gmail.com>
2013-12-16 22:52:44 -0500

    test ECDH

---
+++ test/test_dh.js
@@ -0,0 +1,28 @@
+/** 
+ * Test Group Key Exchanges
+ */
+var should = require('should');
+var Sodium = require('../lib/sodium');
+if (process.env.COVERAGE) {
+	Sodium = require('../lib-cov/sodium');
+}
+
+describe('DH Group Key Exchange', function () {
+	it('should work for a groupd', function (done) {
+
+		var a = new Sodium.Key.ECDH("", Sodium.Hash.sha256(new Buffer(
+			"9549937362")));
+		var b = new Sodium.Key.ECDH("", Sodium.Hash.sha256(new Buffer(
+			"9542584444")));
+
+		var abDH = new Sodium.ECDH(b.pk(), a.sk()).secret();
+		var baDH = new Sodium.ECDH(a.pk(), b.sk()).secret();
+
+		console.log(abDH.toString('hex'));
+		console.log(baDH.toString('hex'));
+		abDH.should.eql(baDH);
+
+
+		done();
+	});
+});
