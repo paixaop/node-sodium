2fd9314c2387fadcf78717e6691cca1a780e7dc5
Pedro Paixao <pedro@UM-Book.local>
2016-04-03 23:11:08 -0400

    tests

--- test/test_crypto_generic_hash.js
+++ test/test_crypto_generic_hash.js
@@ -888,5 +888,63 @@ describe('Generic Hash', function() {
         
         done();
     });
+    
+    it('should throw if key size is too big', function(done) {
+        var testMessage = toBuffer(testVectors[0].in);
+        var testKey = new Buffer(sodium.crypto_generichash_KEYBYTES_MAX + 1);
+        testKey.fill(1);
+        
+        assert.throws(
+            () => {
+                var out = sodium.crypto_generichash(sodium.crypto_generichash_BYTES_MIN, testMessage, testKey);
+            },
+            /generichash key size cannot be bigger than/
+        );
+        
+        done();
+    });
+    
+    it('should throw if key size is too small', function(done) {
+        var testMessage = toBuffer(testVectors[0].in);
+        var testKey = new Buffer(sodium.crypto_generichash_KEYBYTES_MIN - 1);
+        testKey.fill(1);
+        
+        assert.throws(
+            () => {
+                var out = sodium.crypto_generichash(sodium.crypto_generichash_BYTES_MIN, testMessage, testKey);
+            },
+            /generichash key size cannot be smaller than/
+        );
+        
+        done();
+    });
+    
+    it('should return match all test vectors using the streaming API', function(done) {
+        testVectors.forEach(function(vector, i) {
+            var halfMessageSize = vector.in.length / 2;
+            
+            var testMessage = toBuffer(vector.in);
+            
+            var testKey = toBuffer(vector.key);
+            
+            var state = sodium.crypto_generichash_init(testKey, sodium.crypto_generichash_BYTES_MAX);
+            
+            var m1 = testMessage.slice(0, testMessage.length / 2);
+            var m2 = testMessage.slice(testMessage.length / 2);
+            
+            if( m1.length ) {
+                sodium.crypto_generichash_update(state, m1);
+            }
+            if( m2.length ) {
+                sodium.crypto_generichash_update(state, m2);
+            }
+            
+            var out = sodium.crypto_generichash_final(state, sodium.crypto_generichash_BYTES_MAX);
+            var expectedOut = toBuffer(vector.out);
+            
+            assert(sodium.compare(out, expectedOut)==0, "Test vector " + i + " failed." );
+        });
+        done();
+    });
 });
     
