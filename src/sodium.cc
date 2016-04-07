3e5bb4e5dbef438b32eef8a7448128062f2b749f
Pedro Paixao <pedro@UM-Book.local>
2016-04-07 16:58:39 -0400

    move to src dir

--- src/sodium.cc
+++ src/sodium.cc
@@ -5,25 +5,8 @@
  * @email paixaop at gmail dot com
  * @License MIT
  */
-#include <node.h>
-#include <node_buffer.h>
-
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
-#include <string>
-#include <sstream>
-
-#include <nan.h>
-
-#include "sodium.h"
-
-using namespace node;
-using namespace v8;
-
-
-// As per Libsodium install docs
-#define SODIUM_STATIC
+#include "node_sodium.h"
+#include "node_sodium_register.h"
 
 // get handle to the global object
 Local<Object> globalObj = Nan::GetCurrentContext()->Global();
@@ -32,75 +15,6 @@ Local<Object> globalObj = Nan::GetCurrentContext()->Global();
 Local<Function> bufferConstructor =
        Local<Function>::Cast(globalObj->Get(Nan::New<String>("Buffer").ToLocalChecked()));
 
-
-// Check if a function argument is a node Buffer. If not throw V8 exception
-#define ARG_IS_BUFFER(i,msg) \
-    if (!Buffer::HasInstance(info[i])) { \
-        std::ostringstream oss; \
-        oss << "argument " << msg << " must be a buffer"; \
-        return Nan::ThrowError(oss.str().c_str()); \
-    }
-
-// Create a new buffer, and get a pointer to it
-#define NEW_BUFFER_AND_PTR(name, size) \
-    Local<Object> name = Nan::NewBuffer(size).ToLocalChecked(); \
-    unsigned char* name ## _ptr = (unsigned char*)Buffer::Data(name)
-
-#define GET_ARG_AS(i, NAME, TYPE) \
-    ARG_IS_BUFFER(i,#NAME); \
-    TYPE NAME = (TYPE) Buffer::Data(info[i]->ToObject()); \
-    unsigned long long NAME ## _size = Buffer::Length(info[i]->ToObject()); \
-    if( NAME ## _size == 0 ) { \
-        std::ostringstream oss; \
-        oss << "argument " << #NAME << " length cannot be zero" ; \
-        return Nan::ThrowError(oss.str().c_str()); \
-    }
-
-#define GET_ARG_AS_LEN(i, NAME, MAXLEN, TYPE) \
-    GET_ARG_AS(i, NAME, TYPE); \
-    if( NAME ## _size != MAXLEN ) { \
-        std::ostringstream oss; \
-        oss << "argument " << #NAME << " must be " << MAXLEN << " bytes long" ; \
-        return Nan::ThrowError(oss.str().c_str()); \
-    }
-
-#define GET_ARG_AS_UCHAR(i, NAME) \
-    GET_ARG_AS(i, NAME, unsigned char*)
-
-#define GET_ARG_AS_UCHAR_LEN(i, NAME, MAXLEN) \
-    GET_ARG_AS_LEN(i, NAME, MAXLEN, unsigned char*)
-
-#define GET_ARG_AS_VOID(i, NAME) \
-    GET_ARG_AS(i, NAME, void*)
-
-#define GET_ARG_AS_VOID_LEN(i, NAME, MAXLEN) \
-    GET_ARG_AS_LEN(i, NAME, MAXLEN, void*)
-
-#define GET_ARG_NUMBER(i, NAME) \
-    size_t NAME; \
-    if (info[i]->IsUint32()) { \
-        NAME = info[i]->Int32Value(); \
-    } else { \
-        return Nan::ThrowError("argument size must be a number"); \
-    }
-
-#define GET_ARG_POSITIVE_NUMBER(i, NAME) \
-    GET_ARG_NUMBER(i, NAME); \
-    if( NAME < 0 ) { \
-        return Nan::ThrowError("argument size must be a positive number"); \
-    }
-
-#define NUMBER_OF_MANDATORY_ARGS(n, message) \
-    if (info.Length() < (n)) {               \
-        return Nan::ThrowError(message);       \
-    }
-
-#define TO_REAL_BUFFER(slowBuffer, actualBuffer) \
-    Handle<Value> constructorArgs ## slowBuffer[3] = \
-        { slowBuffer->handle_, \
-          Nan::New<Integer>(Buffer::Length(slowBuffer)), \
-          Nan::New<Integer>(0) }; \
-    Local<Object> actualBuffer = bufferConstructor->NewInstance(3, constructorArgs ## slowBuffer);
 
 // Lib Sodium Version Functions
 NAN_METHOD(bind_sodium_version_string) {
@@ -1814,101 +1728,6 @@ NAN_METHOD(bind_crypto_generichash_final) {
     } 
 }
 
-/**
- * “int crypto_pwhash_scryptsalsa208sha256(unsigned char * const out,
-                                       unsigned long long outlen,
-                                       const char * const passwd,
-                                       unsigned long long passwdlen,
-                                       const unsigned char * const salt,
-                                       unsigned long long opslimit,
-                                       size_t memlimit);”
-
-    number out length
-    buffer passwd
-    buffer salt
-    number opslimit
-    number memlimit
- */
-NAN_METHOD(bind_crypto_pwhash_scryptsalsa208sha256) {
-    Nan::EscapableHandleScope scope;
-
-    NUMBER_OF_MANDATORY_ARGS(5,"arguments must be: output length, password buffer, salt buffer, oLimit, memLimit");
-    
-    GET_ARG_POSITIVE_NUMBER(0, out_size);
-    GET_ARG_AS(1, passwd, char*);
-    GET_ARG_AS_UCHAR_LEN(2, salt, crypto_pwhash_scryptsalsa208sha256_SALTBYTES);
-    GET_ARG_POSITIVE_NUMBER(3, oppLimit);
-    GET_ARG_POSITIVE_NUMBER(4, memLimit);
-    
-    NEW_BUFFER_AND_PTR(hash, out_size);
-    
-    if (crypto_pwhash_scryptsalsa208sha256(hash_ptr, out_size, passwd, passwd_size, salt, oppLimit, memLimit) == 0) {
-        return info.GetReturnValue().Set(hash);
-    } else {
-        return info.GetReturnValue().Set(Nan::Null());
-    } 
-}
-
-/**
-  int crypto_pwhash_scryptsalsa208sha256_str(char out[crypto_pwhash_scryptsalsa208sha256_STRBYTES],
-                                           const char * const passwd,
-                                           unsigned long long passwdlen,
-                                           unsigned long long opslimit,
-                                           size_t memlimit);
-                                           
-                                           
- */
-NAN_METHOD(bind_crypto_pwhash_scryptsalsa208sha256_str) {
-    Nan::EscapableHandleScope scope;
-
-    NUMBER_OF_MANDATORY_ARGS(3,"arguments must be: password buffer, oLimit, memLimit");
-    
-    GET_ARG_AS(0, passwd, char*);
-    GET_ARG_POSITIVE_NUMBER(1, oppLimit);
-    GET_ARG_POSITIVE_NUMBER(2, memLimit);
-    
-    NEW_BUFFER_AND_PTR(hash, crypto_pwhash_scryptsalsa208sha256_STRBYTES);
-    
-    if (crypto_pwhash_scryptsalsa208sha256_str((char*)hash_ptr, passwd, passwd_size, oppLimit, memLimit) == 0) {
-        return info.GetReturnValue().Set(hash);
-    } else {
-        return info.GetReturnValue().Set(Nan::Null());
-    } 
-}
-
-/**
- * int crypto_pwhash_scryptsalsa208sha256_str_verify(const char str[crypto_pwhash_scryptsalsa208sha256_STRBYTES],
-                                                  const char * const passwd,
-                                                  unsigned long long passwdlen);
- */
-NAN_METHOD(bind_crypto_pwhash_scryptsalsa208sha256_str_verify) {
-    Nan::EscapableHandleScope scope;
-
-    NUMBER_OF_MANDATORY_ARGS(2,"arguments must be: pwhash string, password");
-    
-    GET_ARG_AS_UCHAR_LEN(0, hash, crypto_pwhash_scryptsalsa208sha256_STRBYTES);
-    GET_ARG_AS(1, passwd, char*);
-    
-    if (crypto_pwhash_scryptsalsa208sha256_str_verify((char*)hash, passwd, passwd_size) == 0) {
-        return info.GetReturnValue().Set(Nan::True());
-    } else {
-        return info.GetReturnValue().Set(Nan::False());
-    } 
-}
-
-#define NEW_INT_PROP(NAME) \
-    Nan::ForceSet(target, Nan::New<String>(#NAME).ToLocalChecked(), Nan::New<Integer>(NAME), v8::ReadOnly);
-
-#define NEW_NUMBER_PROP(NAME) \
-    Nan::ForceSet(target, Nan::New<String>(#NAME).ToLocalChecked(), Nan::New<Number>(NAME), v8::ReadOnly);
-
-
-#define NEW_STRING_PROP(NAME) \
-    Nan::ForceSet(target, Nan::New<String>(#NAME).ToLocalChecked(), Nan::New<String>(NAME).ToLocalChecked(), v8::ReadOnly);
-
-#define NEW_METHOD(NAME) \
-    Nan::SetMethod(target, #NAME, bind_ ## NAME)
-
 void RegisterModule(Handle<Object> target) {
     // init sodium library before we do anything
     if( sodium_init() == -1 ) {
@@ -2062,26 +1881,7 @@ void RegisterModule(Handle<Object> target) {
     NEW_INT_PROP(crypto_generichash_KEYBYTES_MIN);
     NEW_INT_PROP(crypto_generichash_KEYBYTES_MAX);
     
-    NEW_METHOD(crypto_pwhash_scryptsalsa208sha256);
-    NEW_METHOD(crypto_pwhash_scryptsalsa208sha256_str);
-    NEW_METHOD(crypto_pwhash_scryptsalsa208sha256_str_verify);
-    NEW_NUMBER_PROP(crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE);
-    NEW_NUMBER_PROP(crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE);
-    NEW_NUMBER_PROP(crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_SENSITIVE);
-    NEW_NUMBER_PROP(crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_SENSITIVE);
-    NEW_INT_PROP(crypto_pwhash_scryptsalsa208sha256_SALTBYTES);
-    NEW_INT_PROP(crypto_pwhash_scryptsalsa208sha256_STRBYTES);
-    NEW_STRING_PROP(crypto_pwhash_scryptsalsa208sha256_STRPREFIX);
-    NEW_NUMBER_PROP(crypto_pwhash_ALG_DEFAULT);
-    NEW_NUMBER_PROP(crypto_pwhash_SALTBYTES);
-    NEW_NUMBER_PROP(crypto_pwhash_STRBYTES);
-    NEW_STRING_PROP(crypto_pwhash_STRPREFIX);
-    NEW_NUMBER_PROP(crypto_pwhash_OPSLIMIT_INTERACTIVE);
-    NEW_NUMBER_PROP(crypto_pwhash_MEMLIMIT_INTERACTIVE);
-    NEW_NUMBER_PROP(crypto_pwhash_OPSLIMIT_MODERATE);
-    NEW_NUMBER_PROP(crypto_pwhash_MEMLIMIT_MODERATE);
-    NEW_NUMBER_PROP(crypto_pwhash_OPSLIMIT_SENSITIVE);
-    NEW_NUMBER_PROP(crypto_pwhash_MEMLIMIT_SENSITIVE);
+    register_pwhash(target);
 
     // Scalar Mult
     NEW_METHOD(crypto_scalarmult);
