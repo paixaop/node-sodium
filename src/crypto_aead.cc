82d89faf9682873612ecdba6573d9fcaeee0f0b8
Pedro Paixao <pedro@UM-Book.local>
2016-04-17 01:53:40 -0400

    document API

--- src/crypto_aead.cc
+++ src/crypto_aead.cc
@@ -1,4 +1,4 @@
-/**
+/*
  * Node Native Module for Lib Sodium
  *
  * @Author Pedro Paixao
@@ -8,56 +8,148 @@
 #include "node_sodium.h"
 #include "crypto_aead.h"
 
+/***
+ * Authenticated Encryption with Additional Data:
+ *
+ * Encrypts a message with a key and a nonce to keep it confidential Computes
+ * an authentication tag. This tag is used to make sure that the message, as
+ * well as optional, non-confidential (non-encrypted) data, haven't been
+ * tampered with.
+ *
+ * A typical use case for additional data is to store protocol-specific metadata
+ * about the message, such as its length and encoding.
+ */
+
+/**
+ * Crypto AEAD AES 256 GCM:
+ *
+ * The current implementation of this construction is hardware-accelerated and
+ * requires the Intel SSSE3 extensions, as well as the aesni and pclmul
+ * instructions.
+ *
+ * Intel Westmere processors (introduced in 2010) and newer meet the requirements.
+ *
+ * There are no plans to support non hardware-accelerated implementations of
+ * AES-GCM. If portability is a concern, use ChaCha20-Poly1305 instead.
+ */
+
+/** Low Level API: */
+/**
+ * crypto_aead_aes256gcm_is_available:
+ *
+ * Check hardware support for AES 256 GCM
+ *
+ * **Returns**:
+ *
+ * ~ true: if hardware supports AES 256 GCM
+ *
+ * **Sample**:
+ *
+ *   if( sodium.crypto_aead_aes256gcm_is_available() ) {
+ *     // You can use the crypto_aead_aes256gcm_*()
+ *   }
+ *   else {
+ *     // Use crypto_aead_chacha20poly1305_*()
+ *   }
+ *
+ */
 NAN_METHOD(bind_crypto_aead_aes256gcm_is_available) {
     Nan::EscapableHandleScope scope;
 
     if( crypto_aead_aes256gcm_is_available() == 1 ) {
         return info.GetReturnValue().Set(Nan::True());
     }
-    
+
     return info.GetReturnValue().Set(Nan::False());
 }
 
 
-/* int crypto_aead_aes256gcm_beforenm(crypto_aead_aes256gcm_state *ctx_,
-                                   const unsigned char *k);
-*/
-NAN_METHOD(bind_crypto_aead_aes256gcm_beforenm) { 
+/**
+ * crypto_aead_aes256gcm_beforenm:
+ * Precompute AES key expansion.
+ *
+ * Applications that encrypt several messages using the same key can gain a
+ * little speed by expanding the AES key only once, via the precalculation interface
+ * Initializes a context ctx by expanding the key k and always returns 0.
+ *
+ *   var ctx = sodium.crypto_aead_aes256gcm_beforenm(key);
+ *
+ * ~ key (Buffer):  AES 256 GCM Key buffer with crypto_aead_aes256gcm_KEYBYTES in length
+ *
+ * **Sample**:
+ *
+ *     // Generate a random key
+ *     var key = new Buffer(crypto_aead_aes256gcm_KEYBYTES);
+ *     sodium.randombytes_buf(key);
+ *     var state = sodium.crypto_aead_aes256gcm_beforenm(key);
+ */
+NAN_METHOD(bind_crypto_aead_aes256gcm_beforenm) {
     Nan::EscapableHandleScope scope;
-    
+
     ARGS(1,"arguments key must be a buffer");
     ARG_TO_UCHAR_BUFFER_LEN(key, crypto_aead_aes256gcm_KEYBYTES);
-    
+
     NEW_BUFFER_AND_PTR(ctxt, crypto_aead_aes256gcm_statebytes());
-    
+
     if (crypto_aead_aes256gcm_beforenm((crypto_aead_aes256gcm_state*)ctxt_ptr, key) == 0) {
         return info.GetReturnValue().Set(ctxt);
     }
-    
+
     return info.GetReturnValue().Set(Nan::Undefined());
 }
 
-/*
- int crypto_aead_aes256gcm_encrypt_afternm(unsigned char *c,
-                                          unsigned long long *clen_p,
-                                          const unsigned char *m,
-                                          unsigned long long mlen,
-                                          const unsigned char *ad,
-                                          unsigned long long adlen,
-                                          const unsigned char *nsec,
-                                          const unsigned char *npub,
-                                          const crypto_aead_aes256gcm_state *ctx_);
-*/
+/**
+ * crypto_aead_aes256gcm_encrypt_afternm:
+ * Encrypt data
+ *
+ *    var c = sodium.crypto_aead_aes256gcm_encrypt_afternm(
+ *              message,
+ *              additionalData,
+ *              nonce,
+ *              ctx);
+ *
+ * ~ message (Buffer): plain text buffer
+ * ~ additionalData (Buffer): non-confidential data to add to the cipher text. Can be `null`
+ * ~ nonce (Buffer): a nonce with `sodium.crypto_aead_aes256gcm_NPUBBYTES` in length
+ * ~ ctx (Buffer): state computed by `crypto_aead_aes256gcm_beforenm()`
+ *
+ * **Returns**:
+ *
+ * ~ cipherText (Buffer): The encrypted message, as well as a tag authenticating
+ *   both the confidential message `message` and non-confidential data `additionalData`
+ *
+ * **Sample**:
+ *
+ *     // Generate a random key
+ *     var key = new Buffer(crypto_aead_aes256gcm_KEYBYTES);
+ *     sodium.randombytes_buf(key);
+ *
+ *     // Generate random nonce
+ *     var nonce = new Buffer(crypto_aead_aes256gcm_KEYBYTES);
+ *     sodium.randombytes_buf(nonce);
+ *
+ *     // Precompute and generate the state
+ *     var state = sodium.crypto_aead_aes256gcm_beforenm(key);
+ *
+ *     var message = new Buffer("this is a plain text message");
+ *     var additionalData = new Buffer("metadata");
+ *     var cipherText = sodium.crypto_aead_aes256gcm_encrypt_afternm(
+ *        message, additionalData, nonce, state);
+ *
+ */
 NAN_METHOD(bind_crypto_aead_aes256gcm_encrypt_afternm) {
     Nan::EscapableHandleScope scope;
+
     ARGS(4,"arguments message, additional data, nonce, and key must be buffers");
     ARG_TO_UCHAR_BUFFER(m);
     ARG_TO_UCHAR_BUFFER_OR_NULL(ad);
     ARG_TO_UCHAR_BUFFER_LEN(npub, crypto_aead_aes256gcm_NPUBBYTES);
     ARG_TO_VOID_BUFFER_LEN(ctx, crypto_aead_aes256gcm_statebytes());
+
     NEW_BUFFER_AND_PTR(c, crypto_aead_aes256gcm_ABYTES + m_size);
     sodium_memzero(c_ptr, crypto_aead_aes256gcm_ABYTES + m_size);
     unsigned long long clen;
+
     if( crypto_aead_aes256gcm_encrypt_afternm (c_ptr, &clen, m, m_size, ad, ad_size, NULL, npub, (crypto_aead_aes256gcm_state*)ctx) == 0 ) {
         return info.GetReturnValue().Set(c);
     }
@@ -77,7 +169,7 @@ NAN_METHOD(bind_crypto_aead_aes256gcm_encrypt_afternm) {
 */
 NAN_METHOD(bind_crypto_aead_aes256gcm_decrypt_afternm) {
     Nan::EscapableHandleScope scope;
-    
+
     ARGS(4,"arguments chiper text, additional data, nonce, and key must be buffers");
     ARG_TO_UCHAR_BUFFER(c);
     if( c_size < crypto_aead_aes256gcm_ABYTES ) {
@@ -88,14 +180,14 @@ NAN_METHOD(bind_crypto_aead_aes256gcm_decrypt_afternm) {
     ARG_TO_UCHAR_BUFFER_OR_NULL(ad);
     ARG_TO_UCHAR_BUFFER_LEN(npub, crypto_aead_aes256gcm_NPUBBYTES);
     ARG_TO_VOID_BUFFER_LEN(ctx, crypto_aead_aes256gcm_statebytes());
-    
+
     NEW_BUFFER_AND_PTR(m, c_size - crypto_aead_aes256gcm_ABYTES);
     unsigned long long mlen;
-    
+
     if( crypto_aead_aes256gcm_decrypt_afternm (m_ptr, &mlen, NULL, c, c_size, ad, ad_size, npub, (crypto_aead_aes256gcm_state*)ctx) == 0 ) {
         return info.GetReturnValue().Set(m);
     }
-    
+
     return info.GetReturnValue().Set(Nan::Undefined());
 }
 
@@ -113,23 +205,23 @@ int crypto_aead_aes256gcm_encrypt_detached_afternm(unsigned char *c,
                                                    */
 NAN_METHOD(bind_crypto_aead_aes256gcm_encrypt_detached_afternm) {
     Nan::EscapableHandleScope scope;
-    
+
     ARGS(4,"arguments message, additional data, nonce, and key must be buffers");
     ARG_TO_UCHAR_BUFFER(m);
     ARG_TO_UCHAR_BUFFER_OR_NULL(ad);
     ARG_TO_UCHAR_BUFFER_LEN(npub, crypto_aead_aes256gcm_NPUBBYTES);
     ARG_TO_VOID_BUFFER_LEN(ctx, crypto_aead_aes256gcm_statebytes());
-    
+
     NEW_BUFFER_AND_PTR(c, m_size);
     NEW_BUFFER_AND_PTR(mac, crypto_aead_aes256gcm_ABYTES);
-    
+
     if( crypto_aead_aes256gcm_encrypt_detached_afternm(c_ptr, mac_ptr, NULL, m, m_size, ad, ad_size, NULL, npub, (crypto_aead_aes256gcm_state*)ctx) == 0 ) {
         Local<Object> result = Nan::New<Object>();
         result->ForceSet(Nan::New<String>("cipherText").ToLocalChecked(), c, DontDelete);
         result->ForceSet(Nan::New<String>("mac").ToLocalChecked(), mac, DontDelete);
         return info.GetReturnValue().Set(result);
     }
-    
+
     return info.GetReturnValue().Set(Nan::Undefined());
 }
 
@@ -146,20 +238,20 @@ int crypto_aead_aes256gcm_decrypt_detached_afternm(unsigned char *m,
 */
 NAN_METHOD(bind_crypto_aead_aes256gcm_decrypt_detached_afternm) {
     Nan::EscapableHandleScope scope;
-    
+
     ARGS(4,"arguments cipher message, mac, additional data, nsec, nonce, and key must be buffers");
     ARG_TO_UCHAR_BUFFER(c);
     ARG_TO_UCHAR_BUFFER_LEN(mac, crypto_aead_aes256gcm_ABYTES);
     ARG_TO_UCHAR_BUFFER_OR_NULL(ad);
     ARG_TO_UCHAR_BUFFER_LEN(npub, crypto_aead_aes256gcm_NPUBBYTES);
     ARG_TO_VOID_BUFFER_LEN(ctx, crypto_aead_aes256gcm_statebytes());
-    
+
     NEW_BUFFER_AND_PTR(m, c_size);
-    
+
     if( crypto_aead_aes256gcm_decrypt_detached_afternm(m_ptr, NULL, c, c_size, mac, ad, ad_size, npub, (crypto_aead_aes256gcm_state*)ctx) == 0 ) {
         return info.GetReturnValue().Set(m);
     }
-    
+
     return info.GetReturnValue().Set(Nan::Undefined());
 }
 
