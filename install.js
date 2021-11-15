2c2307ed1386a07b480fbd6f299975dc5725aa94
ducktrshessami <jwee761@gmail.com>
2021-11-15 14:55:55 -0800

    support vs 2022

--- install.js
+++ install.js
@@ -158,7 +158,8 @@ function getPlatformToolsVersion() {
         2013: 'v120',
         2015: 'v140',
         2017: 'v141',
-        2019: 'v141'
+        2019: 'v141', // v142
+        2022: 'v141'  // v143
     }
 
     checkMSVSVersion();
@@ -306,7 +307,7 @@ function errorSetMSVSVersion() {
     console.log('        npm config set msvs_version 2015\n');
     console.log('    Global:\n');
     console.log('        npm config set msvs_version 2015 --global\n');
-    console.log('Supported values are 2010, 2012, 2013, 2015, 2017, 2019\n');
+    console.log('Supported values are 2010, 2012, 2013, 2015, 2017, 2019, 2022\n');
     process.exit(1);
 }
 
@@ -318,7 +319,7 @@ function errorInvalidMSVSVersion() {
     console.log('        npm config set msvs_version 2015\n');
     console.log('    Global:\n');
     console.log('        npm config set msvs_version 2015 --global\n');
-    console.log('Supported values are 2010, 2012, 2013, 2015, 2017, 2019\n');
+    console.log('Supported values are 2010, 2012, 2013, 2015, 2017, 2019, 2022\n');
     process.exit(1);
 }
 
@@ -327,7 +328,7 @@ function checkMSVSVersion() {
         errorSetMSVSVersion();
     }
     console.log('MS Version: ' + process.env.npm_config_msvs_version);
-    if (process.env.npm_config_msvs_version.search(/^2010|2012|2013|2015|2017|2019$/)) {
+    if (process.env.npm_config_msvs_version.search(/^2010|2012|2013|2015|2017|2019|2022$/)) {
         errorInvalidMSVSVersion();
     }
 }
